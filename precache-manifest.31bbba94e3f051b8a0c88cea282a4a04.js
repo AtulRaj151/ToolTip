db67478e5b1293a814b5773e7274b4b6284e3d68
Atul Raj <atulsid151@gmail.com>
2020-08-10 15:58:24 +0530

    Updates

---
+++ precache-manifest.31bbba94e3f051b8a0c88cea282a4a04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "764771e3cb1ec1d0f2044f6c9da595bd",
+    "url": "/ToolTip/index.html"
+  },
+  {
+    "revision": "80289d9668f6a982ac2b",
+    "url": "/ToolTip/static/css/main.364217da.chunk.css"
+  },
+  {
+    "revision": "8b3402b2317bac2f07af",
+    "url": "/ToolTip/static/js/2.7ddb2575.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToolTip/static/js/2.7ddb2575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80289d9668f6a982ac2b",
+    "url": "/ToolTip/static/js/main.679a0824.chunk.js"
+  },
+  {
+    "revision": "23c52bcac866d16b853c",
+    "url": "/ToolTip/static/js/runtime-main.82592fd5.js"
+  }
+]);
